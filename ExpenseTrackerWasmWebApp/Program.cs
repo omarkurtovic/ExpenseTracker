c3840a62d4ac6c9288109d91e5d34168baa27fd6
omar <omarkurtovic19@gmail.com>
2026-02-04 15:44:17 +0100

    Small ux fixes for snackbar and confirm dialog.

--- ExpenseTrackerWasmWebApp/Program.cs
+++ ExpenseTrackerWasmWebApp/Program.cs
@@ -57,11 +57,11 @@ void ConfigureMudBlazor(IServiceCollection services)
     services.AddMudServices();
     services.AddMudServices(config =>
     {
-        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+        config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopRight;
         config.SnackbarConfiguration.PreventDuplicates = false;
         config.SnackbarConfiguration.NewestOnTop = false;
         config.SnackbarConfiguration.ShowCloseIcon = true;
-        config.SnackbarConfiguration.VisibleStateDuration = 8000;
+        config.SnackbarConfiguration.VisibleStateDuration = 4000;
         config.SnackbarConfiguration.HideTransitionDuration = 500;
         config.SnackbarConfiguration.ShowTransitionDuration = 500;
         config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
